16fa9c18cd450bef95feeaf743ca72f29ae7b0d8
Maciek <maciej.milewski@deliveryhero.com>
2017-06-30 14:36:02 +0200

    add routers

---
+++ app/components/Battle.js
@@ -0,0 +1,15 @@
+var React = require ('react');
+
+class Battle extends React.Component {
+  render () {
+
+    return (
+        <div>
+          Battle
+        </div>
+
+    )
+  }
+}
+
+module.exports = Battle;
